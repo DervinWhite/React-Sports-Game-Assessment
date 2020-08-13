900d607f39e97a20c25ba7da1cd00ccc54bcc50e
Dervin White <dervin.white@gmail.com>
2020-08-13 14:49:06 -0400

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,57 @@
+//WHAT ALL NEEDS TO BE IMPORTED??
+import React from 'react';
+import ReactDOM from 'react-dom'
+import './index.css';
+
+import logo from './logo.svg';
+import './App.css';
+import Scoreboard from './components/scoreboard/Scoreboard'
+import Game from './components/game/Game';
+import Team from  './components/team/Team'
+
+///////////////////////////////
+
+function App(props) {
+  const raccoons = {
+    name: 'Russiaville Raccoons',
+    logoSrc: './assets/images/raccoon.png'
+  }
+
+  const squirrels = {
+    name: 'Sheridan Squirrels',
+    logoSrc: './assets/images/squirrel.png'
+  }
+
+  const bunnies = {
+    name: 'Burlington Bunnies',
+    logoSrc: './assets/images/bunny.png'
+  }
+
+  const hounds = {
+    name: 'Hammond Hounds',
+    logoSrc: './assets/images/hound.png'
+  }
+
+  return (
+    <div className="App">
+      <Game
+        venue="Union 525 Gem"
+        homeTeam={squirrels}
+        visitingTeam={raccoons}
+      />
+      <Game
+        venue="Sheridan Arena"
+        homeTeam={bunnies}
+        visitingTeam={hounds}
+      />
+    </div>
+  )
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
+
+
+export default App;
